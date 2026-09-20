e68b19c484e6963b81773d5cd8ee2377006eab0b
Mohammed Issaqh <mdissaqh2007@gmail.com>
2026-09-20 18:19:31 +0530

    Add user model with password hashing and update dependencies

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "Email already exists"]
+    },
+    password: {
+        type: String,
+    },
+    googleId: {
+        type: String,
+        default: null
+    },
+    profilePicture: {
+        type: String,
+        default: null
+    },
+    authProvider: {
+        type: String,
+        enum: ['local', 'google'],
+        default: 'local'
+    }
+}, { timestamps: true });
+
+userSchema.pre('save', async function () {
+    if (this.isModified('password')) {
+        try {
+            this.password = await bcrypt.hash(this.password, 10);
+        } catch (error) {
+            console.error("Error hashing password:", error);
+        }
+    }
+    else {
+        return;
+    }
+})
+
+const userModel = mongoose.model('User', userSchema);
+
+export default userModel;
